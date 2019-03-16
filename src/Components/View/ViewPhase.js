90ffabca0812aa8090bb0b165a16dcdd04025745
Barry Low <bearcanrun@gmail.com>
2019-03-16 17:51:22 +0100

    Add view phase component

---
+++ src/Components/View/ViewPhase.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Grid, Typography, List } from '@material-ui/core';
+import ExpandItemWrapper from './ExpandItemWrapper';
+import ViewSituation from './ViewSituation';
+
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class ViewPhase extends React.Component {
+  render() {
+    const { classes, index, sectionName } = this.props;
+    const { name, situations } = this.props.phase;
+
+    console.log(this.props.situation);
+    return (
+      <ExpandItemWrapper sectionName={`Phase: ${name}`} style={classes.expansionSituation}>
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} >
+            <List className={classes.root}>
+              {situations.length > 0 &&
+                situations.map((situation, index) => <ViewSituation key={index} name={situation.name} situation={situation.code} index={index} />)
+              }
+            </List>
+          </Grid>
+        </Grid>
+      </ExpandItemWrapper>
+    );
+  }
+}
+
+export default withStyles(styles)(ViewPhase);
