318f167d1bb695fce019558079bcc5757b40605f
Barry Low <bearcanrun@gmail.com>
2019-04-07 17:01:49 +0200

    Docs touchup + 0.0.8

--- src/Components/ViewSpace/docs/introduction.js
+++ src/Components/ViewSpace/docs/introduction.js
@@ -1,8 +1,9 @@
 export const Introduction = `
 ## Welcome to the Strategizer\n\n
-The Strategizer enables interacting with algobots through the use of conditional statements.
-
-The Strategizer connects to a special type of indicator algobot called a simulator. The simulator uses the conditions you make to interact with a variety of other indicator bots used to define "strategy events" that tell a trading algot bot how to act given a specified situation.
-
-
+The Strategizer enables interacting with algobots through the use of conditional statements.\n
+\n
+- The Strategizer connects to a special type of indicator algobot called a simulator.\n
+- The simulator uses the conditions you make to interact with a variety of other indicator bots.\n
+- These conditions you create are defined within a situation.\n
+- ... to be continued. Docs in development!\n
 `;
